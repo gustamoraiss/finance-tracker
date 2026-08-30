01fb6c9e4d0da8352c8f979fb1ce864b97d62d15
Gustavo Morais <gustavombdn@gmail.com>
2026-08-30 12:22:28 -0300

    implementa logica da rota /adicionar com banco de dados

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
@@ -6,8 +7,34 @@ app = Flask(__name__)
 def index():
     return "Funcionando"
 
-@app.route('/adicionar')
+@app.route('/adicionar', methods=['GET','POST'])
 def adicionar():
+    if request.method == 'POST':
+
+        valor = request.form['valor']
+        tipo = request.form['tipo']
+        categoria = request.form['categoria']
+        data = request.form['data']
+        descricao = request.form['descricao']
+
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+
+        cursor.execute('''
+            INSERT INTO transacoes(
+            valor,
+            tipo,
+            categoria,
+            data,
+            descricao)
+            VALUES (?,?,?,?,?)
+            ''', (valor, tipo, categoria, data, descricao))
+        conn.commit()
+
+        conn.close()
+
+        return redirect(url_for('index'))
+
     return render_template('adicionar.html')
 
 if __name__ == '__main__':
