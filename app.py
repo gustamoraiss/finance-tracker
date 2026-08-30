f0b877cae8357ebf21d2d923548db4a011156bf0
Gustavo Morais <gustavombdn@gmail.com>
2026-08-30 11:14:30 -0300

    configura rota /adicionar no app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "Funcionando"
+
+@app.route('/adicionar')
+def adicionar():
+    return render_template('adicionar.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
